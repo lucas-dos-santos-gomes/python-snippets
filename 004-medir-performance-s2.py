e07fca7f6d87eb29a7da82962e54bb0f801e6a9c
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-01-12 00:10:42 -0300

    Adding explanation comment

--- 004-medir-performance-s2.py
+++ 004-medir-performance-s2.py
@@ -4,3 +4,5 @@ import timeit
 time_test = timeit.repeat("[(a, b) for a in (1, 3, 5) for b in (2, 4, 6)]")
 print("\nLista de tempos de execução:", time_test)
 print("Tempo mínimo entre as repetições:", min(time_test), "\n")
+
+# A função timeit.repeat executa a instrução passada como argumento várias vezes e retorna uma lista com os tempos de execução de cada execução. O min é usado para obter o tempo mínimo entre as repetições, o que geralmente é uma medida mais confiável do tempo de execução.
